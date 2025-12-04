b30a1d346a4a45faf82baee31bc35a83dd0ac884
Noah Thomas <33853029+noahthomas88@users.noreply.github.com>
2025-12-04 09:25:29 +0000

    removing unused count code

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
 import './App.css'
 import HomePage from './pages/HomePage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <HomePage/>
   )
