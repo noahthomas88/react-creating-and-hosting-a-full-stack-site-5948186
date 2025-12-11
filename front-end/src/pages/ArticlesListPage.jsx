f479c04df4b68baffa660ae76149cc3f25728df7
Noah Thomas <33853029+noahthomas88@users.noreply.github.com>
2025-12-11 09:06:11 +0000

    adding article links to the article list page

--- front-end/src/pages/ArticlesListPage.jsx
+++ front-end/src/pages/ArticlesListPage.jsx
@@ -1,5 +1,17 @@
+import {Link} from "react-router-dom";
+import articles from "../article-content";
+
 export default function ArticlesListPage() {
   return (
-    <h1>This is the Articles list page</h1>
+    <>
+    <h1>Articles</h1>
+    {articles.map(a=> (
+      <Link key={a.name} to={'/articles/' + a.name}>
+      <h3>{a.title}</h3>
+      <p>{a.content[0].substring(0,150)}</p>
+    </Link>
+    ))}
+    </>
+
   );
 }
