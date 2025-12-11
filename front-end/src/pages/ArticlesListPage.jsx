163b32c7abe1e76121423b810516b3a4d3465649
Noah Thomas <33853029+noahthomas88@users.noreply.github.com>
2025-12-11 09:12:39 +0000

    refactoring article list page to pull out article list reusable component.

--- front-end/src/pages/ArticlesListPage.jsx
+++ front-end/src/pages/ArticlesListPage.jsx
@@ -1,16 +1,11 @@
-import {Link} from "react-router-dom";
 import articles from "../article-content";
+import ArticlesList from "../ArticleList";
 
 export default function ArticlesListPage() {
   return (
     <>
     <h1>Articles</h1>
-    {articles.map(a=> (
-      <Link key={a.name} to={'/articles/' + a.name}>
-      <h3>{a.title}</h3>
-      <p>{a.content[0].substring(0,150)}</p>
-    </Link>
-    ))}
+    <ArticlesList articles= {articles} />
     </>
 
   );
